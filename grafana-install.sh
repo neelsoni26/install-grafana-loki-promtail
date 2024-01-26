48c832cab48e533ee72bdf5be2ca6e79d2c8b62e
Neel Soni <36571880+neelsoni26@users.noreply.github.com>
2024-01-26 16:08:23 +0530

    Installation file added

---
+++ grafana-install.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y software-properties-common wget
+sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | 
+sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update #Update the system
+sudo apt-get install grafana -y
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
+sudo systemctl status grafana-server
