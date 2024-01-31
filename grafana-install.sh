ddd560a52f4801ee451e8288397b4d632d8bc25e
Neel Soni <36571880+neelsoni26@users.noreply.github.com>
2024-01-31 22:52:39 +0530

    Update grafana-install.sh

--- grafana-install.sh
+++ grafana-install.sh
@@ -21,5 +21,5 @@ wget https://raw.githubusercontent.com/grafana/loki/v2.8.0/clients/cmd/promtail/
 docker run -d --name loki -v $(pwd):/mnt/config -p 3100:3100 grafana/loki:2.8.0 --config.file=/mnt/config/loki-config.yaml
 docker run -d --name promtail -v $(pwd):/mnt/config -v /var/log:/var/log --link loki grafana/promtail:2.8.0 --config.file=/mnt/config/promtail-config.yaml
 docker ps
-echo "Grafana with Loki and Promtail is installed and configured."
-echo "OPEN PORT 3000 and 3100 FROM SECURITY GROUP"
+echo "=== Congratulations!!! Grafana with Loki and Promtail are installed and configured. ==="
+echo "=== OPEN PORT 3000 and 3100 FROM SECURITY GROUP ==="
